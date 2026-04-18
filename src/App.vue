c3a87af325ec5b098412a788bb47e46f5b6cf7a9
rpshukla973 <surya.prakash973@gmail.com>
2026-04-19 01:18:21 +0530

    Create App.vue file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
+</template>
+<script>
+import Navbar from './components/Navbar.vue';
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+<style>
+body {
+  background-color: #121212;
+  color: #ffffff;
+}
+</style>
